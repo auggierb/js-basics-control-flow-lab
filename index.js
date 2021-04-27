9d1f882265cf2a40f47352d1af7c58bc5117781e
August <auggierutter@gmail.com>
2021-04-27 14:53:42 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+  return ('This one is on me!')
+}
+  if (feet <= 2499){
+  return ('I will gladly take your thirty bucks.')
+}
+else if (feet >= 2501)
+   return ('No can do.');
+ }
+
+  // Write your code here! 
+
+
+function ternaryCheckCity(city){
   // Write your code here!
+  let NYC ;
+  return city === 'NYC' ? 'Ok, sounds good.': 'No go.'
+  
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case 'generous': return  'Thank you so much.'
+    break ;
+    case 'not as generous': return  'Thank you.'
+    break ;
+    default: return 'Bye.'
+    
+  }
 }
